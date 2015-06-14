866c83d9ede5040ef3c1e8eabbd8651179a78ef7
00mathieu <00mathieu@gmail.com>
2015-06-14 17:56:43 +1000

    altered example for inv matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatix is a function that stores a list of four functions:
+##-set -- changes the matrix stored in the main function
+##-get -- returns the matrix x stored in the main function
+##-setinv -- changes the matrix stored as the inverse
+##-getinv -- returns the matrix stored as the invers
 
 makeCacheMatrix <- function(x = matrix()) {
-  asdf<-3
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(inv) m <<- inv
+    getinv <- function() m
+    list(set = set, get = get,
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-  ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
